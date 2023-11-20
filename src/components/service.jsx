2566763566c7a2f7812b18a45f9abdc62ba5c18a
Nana <nanakurnianingsih82716@gmail.com>
2023-11-20 22:32:08 +0700

    feat: service

---
+++ src/components/service.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function HomeService() {
+  return (
+    <div>
+      <div>
+        <h1>
+          Kategori <span>Layanan Utama</span>
+        </h1>
+        <h1>Kami</h1>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default HomeService
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../components/navbar'
 import { Route, Routes } from 'react-router-dom';
 import HomeGet from '../components/homeget';
+import HomeService from '../components/service';
 
 function Home() {
   return (
     <>
       <HomeGet/>
+      <HomeService/>
     </>
   );
 }
