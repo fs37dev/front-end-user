525f32e4b19d67a333cc1ee881173ca741ad3004
unknown <heryrhamadan@gmail.com>
2023-11-27 02:20:00 +0700

    refactor: success get all doctors

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/service" element={<HomeService />} />
+        {/* <Route path="/service" element={<HomeService />} />
         <Route path="/doctors" element={<BookDokter />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/detaildokter" element={<DetailDokter />} />
+        <Route path="/detaildokter" element={<DetailDokter />} /> */}
       </Routes>
     </>
   );
