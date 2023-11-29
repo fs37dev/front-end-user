27207a59dd98d98479310d8a7fd19664cc58694c
unknown <heryrhamadan@gmail.com>
2023-11-30 02:37:45 +0700

    refactor: login & register

--- src/components/inputregister.jsx
+++ src/components/inputregister.jsx
@@ -5,13 +5,13 @@ import { registerUser } from "../redux/actions/user-action";
 
 function InputRegister() {
   const navigate = useNavigate();
+  const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
   const [nama, setNama] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [konfirmasiPassword, setKonfirmasiPassword] = useState("");
   const [error, setError] = useState("");
-  const dispatch = useDispatch();
-  const auth = useSelector((state) => state.auth);
 
   const handleSignUp = (e) => {
     e.preventDefault();
@@ -24,6 +24,7 @@ function InputRegister() {
       hideErrorAfterTimeout();
     } else {
       dispatch(registerUser(nama, email, password));
+      hideErrorAfterTimeout();
     }
   };
 
@@ -39,17 +40,17 @@ function InputRegister() {
     }
   }, [auth.isAuthenticated, navigate]);
 
+  useEffect(() => {
+    setError(auth.error);
+  }, [auth]);
+
   return (
     <div className="relative w-full h-screen">
       <div className="flex justify-center items-center h-full">
         <form className="rounded-lg shadow-2xl lg:w-full max-w-[900px] w-100 mx-auto bg-white ">
           <div className="card-body">
-            <h1 className="font-bold text-center text-xl">
-              Register Dengan Kami!
-            </h1>
-            <h2 className="font-bold text-center text-base">
-              Informasi Anda aman bersama kami
-            </h2>
+            <h1 className="font-bold text-center text-xl">Register Dengan Kami!</h1>
+            <h2 className="font-bold text-center text-base">Informasi Anda aman bersama kami</h2>
             <div className="flex flex-col py-1">
               <label>Nama</label>
               <input
@@ -90,20 +91,13 @@ function InputRegister() {
                 onChange={(e) => setKonfirmasiPassword(e.target.value)}
               />
             </div>
-            <button
-              className="w-50 py-3 mt-3 rounded-full bg-emerald-500 hover:bg-emerald-700 relative text-white"
-              onClick={handleSignUp}
-            >
+            <button className="w-50 py-3 mt-3 rounded-full bg-emerald-500 hover:bg-emerald-700 relative text-white" onClick={handleSignUp}>
               Sign Up
             </button>
             {error && <p className="text-center text-red-500 mt-2">{error}</p>}
             <p className="text-center mt-3">
               Sudah Punya Akun?{" "}
-              <button 
-              type="button"
-                onClick={() => navigate("/login")}
-                className="text-emerald-500 font-bold"
-              >
+              <button type="button" onClick={() => navigate("/login")} className="text-emerald-500 font-bold">
                 Sign In
               </button>
             </p>
